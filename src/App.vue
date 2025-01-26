7b9ebaf4ab05974cd6eb44b1f69119bf20f14c25
hugo <hugo572g@outlook.com>
2025-01-26 13:34:23 +0800

    adjust ci setting

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue + TestPR" />
+  <HelloWorld msg="Vite + Vue + TestPR3" />
 </template>
 
 <style scoped>
