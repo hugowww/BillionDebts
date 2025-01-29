d3de143ffe490be82d05f04eadad5aa096ac6450
hugo <hugo572g@outlook.com>
2025-01-29 19:11:28 +0800

    加入進度條樣式

--- src/views/business_pages/AgriculturePage.vue
+++ src/views/business_pages/AgriculturePage.vue
@@ -2,12 +2,42 @@
   <div class="agriculture-page">
     <div class="business-item" @click="st.buyBusiness('farm')">
       <div>
-        農 <span>{{ st.getBusinessLevel("farm") }}</span>
+        <div class="d-flex flex-column">
+          <div>
+            農 <span>{{ st.getBusinessLevel("farm") }}</span>
+          </div>
+          <div
+            class="progress"
+            role="progressbar"
+            aria-label="Success striped"
+            aria-valuemin="0"
+            aria-valuemax="100"
+            style="height: 5px">
+            <div
+              class="progress-bar progress-bar-striped bg-success"
+              :style="{ width: progress2 + '%' }"></div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="business-item" @click="st.buyBusiness('seed')">
       <div>
-        種 <span>{{ st.getBusinessLevel("seed") }}</span>
+        <div class="d-flex flex-column">
+          <div>
+            種 <span>{{ st.getBusinessLevel("seed") }}</span>
+          </div>
+          <div
+            class="progress"
+            role="progressbar"
+            aria-label="Success striped"
+            aria-valuemin="0"
+            aria-valuemax="100"
+            style="height: 5px">
+            <div
+              class="progress-bar progress-bar-striped bg-success"
+              :style="{ width: progress1 + '%' }"></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -15,9 +45,26 @@
 
 <script setup lang="ts">
 import { store } from "../../stores/store";
+import { onMounted, ref } from "vue";
 const st = store();
+
+let progress1 = ref(0);
+let progress2 = ref(0);
+onMounted(() => {
+  setInterval(() => {
+    progress1.value += 10;
+    progress1.value = progress1.value % 100;
+  }, 100);
+  setInterval(() => {
+    progress2.value += 5;
+    progress2.value = progress2.value % 100;
+  }, 100);
+});
 </script>
 
 <style scoped>
 @import "../../styles/business.styl";
+.progress {
+  --bs-progress-bar-transition: width 0.1s linear;
+}
 </style>
